16cea42a9183820a298dbd656427336f287fdf05
Weslley26 <wesley.sjesus7@senacsp.edu.br>
2025-01-23 19:55:31 -0300

    ola

--- ex02/Program.cs
+++ ex02/Program.cs
@@ -1,2 +1,9 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿/*
+ 
+Um funcionário deseja contar quantos dias de trabalho ele teve em um mês. Para 
+isso, ele deve inserir os dias trabalhados (de 1 a 31) até que digite 0, que indica que 
+terminou de informar os dias. O aluno deve implementar essa contagem utilizando 
+as estruturas de repetição while, do while e for.
+ 
+ 
+ */
