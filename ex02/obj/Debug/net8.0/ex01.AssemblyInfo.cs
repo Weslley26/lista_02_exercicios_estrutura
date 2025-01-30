d8a4018e0d464ab8ca413c3c52e0e08173ff5acd
Weslley26 <wesley.sjesus7@senacsp.edu.br>
2025-01-30 20:00:22 -0300

    ola

--- ex02/obj/Debug/net8.0/ex01.AssemblyInfo.cs
+++ ex02/obj/Debug/net8.0/ex01.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ex01")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4eb979b5844aac92cabf43ca78bc80efd0e7651f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7f6c83120d1f75298138bf5fce6032805cea7c1b")]
 [assembly: System.Reflection.AssemblyProductAttribute("ex01")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ex01")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
---
+++ ex10/Program.cs
@@ -0,0 +1,40 @@
+﻿/*
+Um vendedor de passagens deseja saber quantas passagens ele vendeu em um 
+dia. O aluno deve pedir ao usuário para inserir o valor das passagens vendidas até 
+que ele digite um valor negativo. O total de passagens vendidas deve ser calculado 
+e apresentado utilizando while, do while e for.
+ 
+ */
+
+int totalPassagens = 0;
+double valorPassagem;
+Console.WriteLine("Digite o valor das passagens (0 para sair): ");
+while (true)
+{
+    valorPassagem = Convert.ToDouble(Console.ReadLine());
+    if (valorPassagem == 0) break;
+    totalPassagens++;
+}
+Console.WriteLine($"Total de passagens vendidas: {totalPassagens}");
+
+//
+totalPassagens = 0;
+do
+{
+    Console.WriteLine("Digite o valor das passagens (valor negativo para sair):");
+    valorPassagem = Convert.ToDouble(Console.ReadLine());
+    if (valorPassagem >= 0) totalPassagens++;
+} while (valorPassagem >= 0);
+Console.WriteLine($"Total de passagens vendidas: {totalPassagens}");
+
+//
+totalPassagens = 0;
+for (; ; )
+{
+    Console.WriteLine("Digite o valor das passagens vendidas(valor negativo para sair):");
+    valorPassagem = Convert.ToDouble(Console.ReadLine());
+    if (valorPassagem == 0) break;
+    totalPassagens++;
+}
+
+Console.WriteLine($"Total de passagens vendidas: {totalPassagens}");
